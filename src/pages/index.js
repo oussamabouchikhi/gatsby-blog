3ce09934a9f7aea0865e80195a75bd20ab16c180
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-08-07 13:45:38 +0100

    Build & display blogs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,43 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+export default ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>Yihua's thoughts</div>
+    <h4>{data.allMarkdownRemark.totalCount}</h4>
+    <div>
+    {
+      data.allMarkdownRemark.edges.map(({node}) => (
+        <div>
+          <span>{ node.frontmatter.title } - { node.frontmatter.date } </span>
+          <p>{ node.excerpt }</p>
+        </div>
+      ))
+    }
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
-export default IndexPage
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            description
+            title
+            date
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
