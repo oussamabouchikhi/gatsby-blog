0677ab4803a93369b944a154c7cea2c2ac0beb50
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-08-07 13:45:06 +0100

    Add markdown & transformer

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,13 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdowns`,
+        path: `${__dirname}/src/markdown-pages`,
+      }
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
