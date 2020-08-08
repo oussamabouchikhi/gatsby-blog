854a3fe6bb9782b4c1d4fbf54c14459b18e9a96b
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-08-08 17:28:12 +0100

    Fix GrapghQL issue

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,17 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`);
 
-exports.onCreate = ({ node, getNode, actions }) => {
+exports.onCreateNode = ({ node, getNode, actions }) => {
     const { createNodeField } = actions
     if (node.internal.type === `MarkdownRemark`) {
-        const slug = createFilePath({ node, getNode })
-
-        createNodeField({
-            node,
-            name: `slug`,
-            value: slug
-        })
-    } 
-}
+      const slug = createFilePath({ node, getNode, basePath: `pages` })
+      createNodeField({
+        node,
+        name: `slug`,
+        value: slug,
+      })
+    }
+  }
 
 exports.createPage = ({ graphql, actions }) => {
     const { createPage } = actions
